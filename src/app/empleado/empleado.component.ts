e306028368c67db013c396d6b4ae9f983fec01b4
Leandro Villalba <leandrovillalba.dev>
2023-09-24 13:15:22 -0300

    interpolación y encapsulamiento de datos

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-empleado',
   templateUrl: './empleado.component.html',
-  styleUrls: ['./empleado.component.css']
+  styleUrls: ['./empleado.component.css'],
 })
 export class EmpleadoComponent {
-
+  nombre = 'Juan';
+  apellido = 'Pérez';
+  private edad = 18; // Se pueden encapsular los datos
+  empresa = 'Píldoras Informáticas';
+  getEdad() {
+    return this.edad; //Mostrar datos a través de getter
+  }
 }
